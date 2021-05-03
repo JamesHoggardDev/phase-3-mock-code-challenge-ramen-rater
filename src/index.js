c8d8bda813ec2b349dcebca0445322590e317430
james <jameshoggard.dev@gmail.com>
2021-05-02 21:28:09 -0400

    fiveC

--- src/index.js
+++ src/index.js
@@ -1,107 +1,110 @@
-renderRamenMenu()
-
-const ramenMenu = document.querySelector("#ramen-menu")
-const detailedRamenUpdateForm = document.querySelector("#ramen-rating")
+const menuDiv = document.querySelector("#ramen-menu")
+const centImg = document.querySelector("#ramen-detail > img")
+const centH2 = document.querySelector("#ramen-detail > h2")
+const centH3 = document.querySelector("#ramen-detail > h3")
+const ratiForm = document.querySelector("#ramen-rating")
 const deleteBttn = document.querySelector("#ramen-rating > button")
+const newRamenForm = document.querySelector("#new-ramen")
 
-
-fetch('http://localhost:3000/ramens/1')
+function dispAllRamens(){
+    fetch('http://localhost:3000/ramens')
     .then(res => res.json())
-    .then(firstObj => {
-
-        const detailImg = document.querySelector('img.detail-image')
-        detailImg.src = firstObj.image
-        detailImg.alt = firstObj.name
-
-        const detailH2 = document.querySelector("#ramen-detail > h2")
-        detailH2.textContent = firstObj.name
-
-        const detailH3 = document.querySelector("#ramen-detail > h3")
-        detailH3.textContent = firstObj.restaurant
-
-        const ratingInput = document.querySelector("#rating")
-        ratingInput.value = firstObj.rating
-
-        const commentInput = document.querySelector("#comment")
-        commentInput.value = firstObj.comment
-
-        detailedRamenUpdateForm.dataset.id = firstObj.id
-    })
-
-    deleteBttn.addEventListener('click', evt => {
-        // console.log(evt.target.dataset.id)
-        const imgtoRemove = document.querySelector(`img[data-id="${evt.target.dataset.id}"]`)
-        console.log(imgtoRemove)
-        
-        fetch(`http://localhost:3000/ramens/${evt.target.dataset.id}`,{
-            method: "DELETE"
-        })
-        .then(res => res.json())
-        .then(emptyObj => {  
-            imgtoRemove.remove();
-        })
-    });
-
-function renderRamenMenu(){
-    fetch("http://localhost:3000/ramens")        
-    .then(resp => resp.json())
     .then(ramenArr => {
-          ramenArr.forEach(renderOneMenuImage)
-    })      
+        runFirstRamen();
+        ramenArr.forEach(ramenObj => dispOneRamen(ramenObj))
+    })
 }
 
-function renderOneMenuImage(ramenObject){
-    const img = document.createElement('img')
-        img.src = ramenObject.image
-        img.alt = ramenObject.name
-        img.dataset.id = ramenObject.id
-        ramenMenu.append(img)
+function runFirstRamen(){
+    fetch('http://localhost:3000/ramens')
+    .then(res => res.json())
+    .then(ramenArr => {
+        makeCentDiv(ramenArr[0]);
+    })
 }
 
-ramenMenu.addEventListener('click', event =>{
-    if(event.target.matches('img')){
+function dispOneRamen(ramenObj){
+    const detImg = document.createElement('img')
+        detImg.dataset.id = ramenObj.id
+        detImg.classList.add('menu-image')
+        detImg.src = ramenObj.image
+        detImg.alt = ramenObj.name
+        detImg.addEventListener('click', evt => {
+            makeCentDiv(ramenObj)
+        })
+    
+    menuDiv.append(detImg)
+}
 
-        fetch(`http://localhost:3000/ramens/${event.target.dataset.id}`)
-            .then(resp => resp.json())
-            .then(ramenObj =>{
-                
-                const detailImg = document.querySelector('img.detail-image')
-                detailImg.src = ramenObj.image
-                detailImg.alt = ramenObj.name
+function makeCentDiv(ramenObj) {
+    centImg.src = ramenObj.image
 
-                const detailH2 = document.querySelector("#ramen-detail > h2")
-                detailH2.textContent = ramenObj.name
+    centH2.textContent = ramenObj.name
+    centH3.textContent = ramenObj.restaurant
+    ratiForm.rating.value = ramenObj.rating
+    ratiForm.comment.value = ramenObj.comment
+    ratiForm.dataset.id = ramenObj.id
+    deleteBttn.dataset.id = ramenObj.id
+}
 
-                const detailH3 = document.querySelector("#ramen-detail > h3")
-                detailH3.textContent = ramenObj.restaurant
+ratiForm.addEventListener('submit', evt => {
+    evt.preventDefault()
 
-                const ratingInput = document.querySelector("#rating")
-                ratingInput.value = ramenObj.rating
+    const rating = ratiForm.rating.value
+    const comment = ratiForm.comment.value
 
-                const commentInput = document.querySelector("#comment")
-                commentInput.value = ramenObj.comment
-
-                detailedRamenUpdateForm.dataset.id = ramenObj.id
-                
-                deleteBttn.dataset.id = ramenObj.id
-                detailedRamenUpdateForm.append(deleteBttn)
-            })
-    }
-})
-
-detailedRamenUpdateForm.addEventListener("submit", event => {
-    event.preventDefault()
-
-    const rating = event.target.rating.value
-    const comment = event.target.comment.value
-
-    fetch(`http://localhost:3000/ramens/${event.target.dataset.id}`, {
+    fetch(`http://localhost:3000/ramens/${evt.target.dataset.id}`, {
         method: "PATCH",
         headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }, 
+            "content-type": "application/json",
+            "accept": 'application/json'
+        },
         body: JSON.stringify({rating, comment})
     })
-    // .then(console.log)
+    .then(res => res.json())
+    .then(ramenObj => {
+        let detImg = document.querySelector(`img[data-id="${ramenObj.id}"]`)
+            detImg.innerHTML = "";
+            detImg.src = ramenObj.image
+            detImg.alt = ramenObj.name
+            detImg.addEventListener('click', evt => {
+            makeCentDiv(ramenObj)
+        })
+    })
+})
+dispAllRamens();
+
+deleteBttn.addEventListener('click', evt => {
+    let detImgToRemove = document.querySelector(`img.menu-image[data-id="${evt.target.dataset.id}"]`)
+    centImg.src = '';
+    detImgToRemove.remove();
+    
+    fetch(`http://localhost:3000/ramens/${evt.target.dataset.id}`,{
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(console.log);
+})
+
+newRamenForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    let newRamen = {
+        name: newRamenForm.name.value,
+        restaurant: newRamenForm.restaurant.value,
+        image: newRamenForm.image.value,
+        rating: newRamenForm.rating.value,
+        comment: newRamenForm.new-comment.value
+    }
+
+    fetch('http://localhost:3000/ramens', {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify(newRamen)
+    })
+    .then(res => res.json())
+    .then(console.log)
 })
